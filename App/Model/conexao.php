6e03caaa6e5842b213b34d2ffa5caa06f60e4cd9
danillo cesar <danillocesar_06@hotmail.com>
2020-06-16 08:21:49 -0300

    Iniciando Crud com PDO

---
+++ App/Model/conexao.php
@@ -0,0 +1,23 @@
+<?php
+
+class Conexao{
+
+    private static $instance;
+
+    //verifica se já existe uma instancia da conexão
+    //se existir, ele retorna a instancia, que é o atributo $instance
+    //se não existir, ele irá criar uma nova
+    public static function getConn(){
+        //utiliza-se o self quando se trabalha com um método estático
+        if(!isset(self::$instance)){
+            //se não houver a instancia, o atributo $instance receberá uma
+            //nova instancia da classe PDO
+            //e já passa os parametros host, nome do banco, charset, usuario e senha
+            self::$instance = new PDO('mysql:host=localhost;dbname=pdo;charset=utf8','root', '');
+        } else{
+            //se já houver a instancia, ela será retornada
+            return self::$instance;
+        }
+
+    }
+}
